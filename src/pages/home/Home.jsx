2f82380781dfbe5fc6145c16dc61247692f06ff7
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2022-11-08 13:09:20 +0100

    css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 // import Header from '../../components/header/header'
 import Banner from '../../components/banner/Banner'
 import ListCard from '../../components/listCard/ListCard'
+import './Home.css'
 // import Footer from '../../components/footer/footer'
 
 function Home() {
@@ -11,8 +12,10 @@ function Home() {
   return (
     <div>
       {/* <Header /> */}
+      <article className='homeContainer'>
       <Banner page="home" />
       <ListCard />
+      </article>
       {/* <Footer /> */}
     </div>
   )
