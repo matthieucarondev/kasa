14d864f68c195bfa76598d85a693653ed5463f6b
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2022-11-15 12:52:55 +0100

    Updates

---
+++ precache-manifest.694e551f9b37f5c761c81e4af24d91a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "322ecfacacd17ae8207a",
+    "url": "/kasa/static/css/main.6c3e7ba7.chunk.css"
+  },
+  {
+    "revision": "322ecfacacd17ae8207a",
+    "url": "/kasa/static/js/main.8621751f.chunk.js"
+  },
+  {
+    "revision": "335d70f16a1c29bf3ee5",
+    "url": "/kasa/static/js/runtime~main.96e85a30.js"
+  },
+  {
+    "revision": "566c31d083f9d0c5b4d9",
+    "url": "/kasa/static/js/2.ad65781f.chunk.js"
+  },
+  {
+    "revision": "25c1808ed7515e12f3b6fcf497bd8ee3",
+    "url": "/kasa/static/media/imgBanner.25c1808e.jpg"
+  },
+  {
+    "revision": "6b63b5917c6b2f452218a36fd515b56e",
+    "url": "/kasa/static/media/about.6b63b591.png"
+  },
+  {
+    "revision": "c2ee4f236942a6a8b1512d860af8ff3e",
+    "url": "/kasa/static/media/logo.c2ee4f23.svg"
+  },
+  {
+    "revision": "abfce2dd693895ddf40afd4953567f78",
+    "url": "/kasa/index.html"
+  }
+];
