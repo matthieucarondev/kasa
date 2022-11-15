7647df5122ae61445e9b286cf024e03283410da1
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2022-11-15 17:07:15 +0100

    Updates

---
+++ precache-manifest.4b7d08c96e231c68e2ec211ab06209b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e3f0bd2799a6fd6f60e",
+    "url": "/kasa/static/css/main.6c3e7ba7.chunk.css"
+  },
+  {
+    "revision": "8e3f0bd2799a6fd6f60e",
+    "url": "/kasa/static/js/main.8cceb02a.chunk.js"
+  },
+  {
+    "revision": "335d70f16a1c29bf3ee5",
+    "url": "/kasa/static/js/runtime~main.96e85a30.js"
+  },
+  {
+    "revision": "566c31d083f9d0c5b4d9",
+    "url": "/kasa/static/js/2.ad65781f.chunk.js"
+  },
+  {
+    "revision": "25c1808ed7515e12f3b6fcf497bd8ee3",
+    "url": "/kasa/static/media/imgBanner.25c1808e.jpg"
+  },
+  {
+    "revision": "6b63b5917c6b2f452218a36fd515b56e",
+    "url": "/kasa/static/media/about.6b63b591.png"
+  },
+  {
+    "revision": "c2ee4f236942a6a8b1512d860af8ff3e",
+    "url": "/kasa/static/media/logo.c2ee4f23.svg"
+  },
+  {
+    "revision": "cc36283a9a4c909437ea72ab86ac5946",
+    "url": "/kasa/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5481515f484019f63a18512adc2c67f.js"
+  "/kasa/precache-manifest.4b7d08c96e231c68e2ec211ab06209b5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/kasa/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
