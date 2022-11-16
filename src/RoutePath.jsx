b0c4917e9f6885d94424773c11c3eec96bdc773d
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2022-11-16 12:28:39 +0100

    index final

---
+++ src/RoutePath.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Header from './components/header/Header'
+import About from './pages/about/About'
+import Error from './pages/error/Error'
+import Lodging from './pages/lodging/Lodging'
+import Footer from './components/footer/Footer'
+import './RoutePath.css'
+
+function RoutesPath() {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about/" element={<About />} />
+          <Route path="/lodging/:id" element={<Lodging />} />
+          <Route path="/error" element={<Error />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
+export default RoutesPath
--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
-import Header from './components/header/Header'
-import About from './pages/about/About'
-import Error from './pages/error/Error'
-import Lodging from './pages/lodging/Lodging'
-import Footer from './components/footer/Footer'
-import './Index.css'
+import RoutePath from "./RoutePath";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about/" element={<About />} />
-        <Route path="/lodging/:id" element={<Lodging />} />
-        <Route path="/error" element={<Error />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+
+      <div className="contenu">
+        <RoutePath />
+      </div>
+ 
   </React.StrictMode>,
   document.getElementById('root')
 )
