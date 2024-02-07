71f5741bed84dbcd7ab22dacc6673a2371474ca7
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2024-02-07 14:18:55 +0100

    mise a jour rating commentaire

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -5,12 +5,15 @@ import './Rating.css'
 
 
 function Star( {type , key}) {
+  //on verifie si le type est red sa sera Redstar sinon sa sera GreyStar 
   const starImage = type === 'red' ? RedStar : GreyStar;
+//Effectuez le rendu de l’image de l’étoile avec le texte alternatif et l’incrustation appropriés
   return <img src={starImage} key={key} alt={`Rating is ${type === 'red' ? '5' : '0'}/5`} />;
 }
 
 
 function Rating({ rating }) {
+  //Calculer le nombre d’étoiles rouges (arrondies à l’unité inférieure) et d’étoiles grises
   const redStarsCount = Math.floor(rating);
   const greyStarsCount = 5 - redStarsCount;
   return (
