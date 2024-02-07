0f78f8340a9079db0b08b469c757c63360612122
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2024-02-07 14:01:57 +0100

    mise a jour rating

--- src/components/rating/Rating.jsx
+++ src/components/rating/Rating.jsx
@@ -3,43 +3,25 @@ import GreyStar from '../../asset/star.png'
 import RedStar from '../../asset/fullStar.png'
 import './Rating.css'
 
-function Rating({ rating }) {
-  // on soustrait le nombre du tableau  moin 5
-  const lodgingNegRating = 5 - rating
-  function greyStars() {
-    // on affiche le nombre d'étoile gris
-    let greyStarsArray = []
-    for (let i = 0; i < lodgingNegRating; i++) {
-      greyStarsArray.push(
-        <img
-          src={GreyStar}
-          key={`Étoile grisée n°${i}`}
-          alt={`Rating is ${rating}/5`}
-        />
-      )
-    }
-    return greyStarsArray
-  }
-  function redStars() {
-    // on affiche le nombre d'étoile rouge
-    let redStarsArray = []
-    for (let j = 0; j < rating; j++) {
-      redStarsArray.push(
-        <img
-          src={RedStar}
-          key={`Étoile rouge n°${j}`}
-          alt={`Rating is ${rating}/5`}
-        />
-      )
-    }
-    return redStarsArray
-  }
 
+function Star( {type , key}) {
+  const starImage = type === 'red' ? RedStar : GreyStar;
+  return <img src={starImage} key={key} alt={`Rating is ${type === 'red' ? '5' : '0'}/5`} />;
+}
+
+
+function Rating({ rating }) {
+  const redStarsCount = Math.floor(rating);
+  const greyStarsCount = 5 - redStarsCount;
   return (
     <article className="lodgingRating">
-      {redStars()}
-      {greyStars()}
+      {[...Array(redStarsCount)].map((_, index) => (
+        <Star type="red" key={`RedStar${index}`} />
+      ))}
+      {[...Array(greyStarsCount)].map((_, index) => (
+        <Star type="grey" key={`GreyStar${index}`} />
+      ))}
     </article>
-  )
+  );
 }
 export default Rating
