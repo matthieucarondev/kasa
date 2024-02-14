af0b32f1d1cd0f0c48d1e581379a756e4620624e
Matthieu Caron <104826273+matthieucarondev@users.noreply.github.com>
2024-02-14 12:08:02 +0100

    mise a jour Error

--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,15 +1,25 @@
 import React, { useEffect } from 'react'
-import Error from '../../components/error/Error'
+import { Link } from 'react-router-dom'
+import './Error.css'
 
-function ErrorPage() {
+function Error() {
   useEffect(() => {
     document.title = 'kasa-erreur404'
   })
   return (
     <section>
-      <Error />
+        <article className="errorHome">
+      <p className="errorTitle">404</p>
+      <p className="errorText">Oups! La page que vous demandez n'existe pas.</p>
+      <article className="LinkHome">
+        {/* crée le lien  de retour a la page d'accueil */}
+        <Link className="link" to={'/'}>
+          Retourner sur la page d’accueil
+        </Link>
+      </article>
+    </article>
     </section>
   )
 }
 
-export default ErrorPage
+export default Error
